bca0d69e4e9998da8bb890c3dd595d80438fd2f0
Caleb Harrelson <caleb@steptwostudios.com>
2020-07-31 14:17:50 -0500

    Rename packages to test ability to deploy

--- native/packages/react-swipeable-views-native/src/SwipeableViews.animated.tsx
+++ native/packages/react-swipeable-views-native/src/SwipeableViews.animated.tsx
@@ -16,7 +16,7 @@ import {
   checkIndexBounds,
   computeIndex,
   getDisplaySameSlide,
-} from 'react-swipeable-views-core';
+} from 'react-17-swipeable-views-core';
 
 const styles = StyleSheet.create({
   root: {
